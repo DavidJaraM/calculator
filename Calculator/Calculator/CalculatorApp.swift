0be6834ef0685efa9ad6c3ff47a0f38f9d507db8
David Jara <davidjaramar@gmail.com>
2022-11-30 13:22:50 -0600

    Initial commit.

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by David Jara on 30/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
